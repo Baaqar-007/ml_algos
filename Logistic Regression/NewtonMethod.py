5801ce0b31349ebce23b6ddf7b29bf1aeb73f29f
Baaqar-007 <baaqarnaqi@gmail.com>
2023-10-19 15:56:49 +0530

    Optimising the function (that doesn't use numpy)

--- Logistic Regression/NewtonMethod.py
+++ Logistic Regression/NewtonMethod.py
@@ -1,48 +1,132 @@
 import numpy as np
+import math 
+
+epsilon = 1e-7
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    x = float(x)
+    if -x > np.log(np.finfo(type(x)).max): # check for overflow
+        return 0.0
+    else:
+        return 1 / (1 + np.exp(-x))
 
-def newtons_method(X, y, num_iters=10):
+    #return .5 * (1 + np.tanh(.5 * x)) # handling larger inputs
+
+def cross_entropy(Y, predictions):
+    return   sum((Y[i]*math.log(predictions[i] + epsilon ) + (1-Y[i])*math.log(1-predictions[i] + epsilon )) for i in range(len(Y)))
+
+def logistic_newtons_method_numpy(X, y, num_iters=10):
 
     m, n = X.shape
-    theta = np.zeros(n)
+    theta = np.zeros(n) # array of n zeroes
 
     for _ in range(num_iters):
-        h = sigmoid(X @ theta)
-        gradient = X.T @ (h - y)
-        hessian = X.T @ np.diag(h * (1 - h)) @ X
-        theta -= np.linalg.inv(hessian) @ gradient
+
+        h = sigmoid(X @ theta) # calculates the predicted values X multiplied with theta; "@" stands for matrix multiplication
+
+        gradient = X.T @ (h - y) 
+
+        """
+        Gradient of the log-likelihood: The gradient of the log-likelihood with respect to the parameters theta is calculated using the formula:
+        ∇J(θ) = XT (hθ(X) − y)
+
+        X is the matrix of feature values,
+        y is the vector of target values,
+        hθ(X) is the vector of predicted probabilities.
+
+        """
+
+        hessian = X.T @ np.diag(h * (1 - h)) @ X # np.diag() creates a diagonal matrix where the diagonal elements are given by h * (1 - h).
+
+        """
+        The Hessian matrix is a square matrix of second-order partial derivatives of a scalar-valued function. In this case, it’s calculated using the formula:
+
+        H = XT.D.X
+        where:
+
+        D is a diagonal matrix where the diagonal elements are given by hθ(X)(1−hθ(X)).
+
+        """
+
+        theta = theta - np.linalg.inv(hessian) @ gradient
 
     return theta
 
 
+def logistic_newtons_method(theta0, theta1, features , target_values , i):
+    m = len(target_values)  # number of training examples
 
-"""
-Sure, let's break down the code:
+    logistic_features = [sigmoid(theta0 + theta1*features[i]) for i in range(m)]
 
-1. `import numpy as np`: This line imports the numpy library and assigns it the alias `np`. Numpy is a Python library used for working with arrays. It also has functions for working in the domain of linear algebra, fourier transform, and matrices.
+    gradient_theta0 = sum((target_values[i] - logistic_features[i]) for i in range(m))
+    gradient_theta1 = sum((target_values[i] - logistic_features[i]) * features[i] for i in range(m))
 
-2. `def sigmoid(x): return 1 / (1 + np.exp(-x))`: This defines a function called `sigmoid` that takes an input `x` and returns the sigmoid of `x`. The sigmoid function maps any value to a value between 0 and 1 and is often used in machine learning for binary classification problems.
+    hessian_theta0 = sum((logistic_features[i] * (1 - logistic_features[i])) for i in range(m))
+    hessian_theta1 = sum((logistic_features[i] * (1 - logistic_features[i])) * (features[i]**2) for i in range(m))
 
-3. `def newtons_method(X, y, num_iters=10):`: This defines a function called `newtons_method` that takes inputs `X`, `y`, and an optional parameter `num_iters` which defaults to 10 if not provided. This function will implement Newton's method for optimization.
+    theta0 = theta0 - gradient_theta0 / (hessian_theta0 + epsilon)
+    theta1 = theta1 - gradient_theta1 / (hessian_theta1 + epsilon)
 
-4. `m, n = X.shape`: This line gets the shape of the array `X` and assigns it to `m` and `n`. Here, `m` is the number of training examples and `n` is the number of features.
+    return theta0,theta1
 
-5. `theta = np.zeros(n)`: This initializes a vector `theta` of size `n` with all zeros. This vector represents the parameters of our model.
+# Sample data (input features and corresponding target values)
+x = list(range(1,20)) # Input features ; x[0] = 1 (dummy)
+y = [1 if i>5 else 0 for i in x ]
 
-6. `for _ in range(num_iters):`: This starts a loop that will run for a number of times equal to `num_iters`.
+print("Features:",x)
 
-7. `h = sigmoid(X @ theta)`: This calculates the hypothesis function for all training examples using the current parameters in `theta`.
+print("Target values:",y)
 
-8. `gradient = X.T @ (h - y)`: This calculates the gradient of the cost function.
 
-9. `hessian = X.T @ np.diag(h * (1 - h)) @ X`: This calculates the Hessian matrix, which is used in Newton's method to find the direction of steepest descent.
+# Initial parameters
+theta0_log = 0.5
+theta1_log = 0.5
 
-10. `theta -= np.linalg.inv(hessian) @ gradient`: This updates our parameters theta by subtracting the product of the inverse of the Hessian matrix and the gradient from it.
+# Learning rate
 
-11. `return theta`: After all iterations are complete, this returns our final parameters stored in theta.
+# Number of iterations
+num_iterations = 10000000
 
-This code implements Newton's method for logistic regression, which is a binary classification algorithm.
+# error tolerance 
+tolerance = 1e-2
 
-"""
+
+prev_cost = float('inf')
+
+for i in range(num_iterations):
+    theta0_log, theta1_log = logistic_newtons_method(theta0_log, theta1_log, x, y, i)
+
+    predictions = [sigmoid(theta0_log + theta1_log*i)for i in x]
+
+    cost = cross_entropy(y,predictions)
+
+    if abs(cost - prev_cost) < tolerance:
+        print(f"Converged at {i} (for Logistic Regression)")
+        break 
+    prev_cost = cost
+
+print(f"Optimized Theta0 for Logistic Regression (Newton's Method):", theta0_log)
+print(f"Optimized Theta1 for Logistic Regression (Newton's Method):", theta1_log)
+
+input_data = list(range(1,20,3))
+correct_output = [1 if i>5 else 0 for i in input_data ]
+# correct_output = [1 if i%2==0 else 0 for i in input_data]
+
+
+predictions = [sigmoid(theta0_log + theta1_log*i) for i in input_data]
+
+print("Input:", input_data)
+print("Correct output:" , correct_output)
+print("Prediction:", predictions)
+
+# Convert probabilities to class labels
+predicted_labels = [1 if p >= 0.5 else 0 for p in predictions]
+
+# Compare predicted and actual class labels
+correct_predictions = [1 if predicted_labels[i] == correct_output[i] else 0 for i in range(len(correct_output))]
+
+# Calculate accuracy
+accuracy = sum(correct_predictions) / len(correct_predictions) *100
+
+print("Accuracy for Logistic Regression:",accuracy,"%")
+
