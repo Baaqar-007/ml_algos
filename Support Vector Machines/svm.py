8bc8aeddd428096699e184e139990416e9b3cb77
Baaqar-007 <baaqarnaqi@gmail.com>
2023-12-11 01:25:20 +0530

    Minor optimisations

--- Support Vector Machines/svm.py
+++ Support Vector Machines/svm.py
@@ -2,10 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Step 1: Define our data
-np.random.seed(np.random.randint(100))
-X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
+np.random.seed(0)
+# Generate the dataset
+X1 = np.random.randn(20, 2) - [2, 2]  # first class
+X2 = np.random.randn(20, 2) + [2, 2]  # second class
+X = np.r_[X1, X2]
+
+Y1 = [-1] * 20  # labels for first class
+Y2 = [1] * 20   # labels for second class
+Y = np.r_[Y1, Y2]
+
+
 # X is a 40x2 array where the first half of the rows are random points roughly centered around (-2, -2), and the second half are random points roughly centered around (2, 2).
-Y = [-1] * 20 + [1] * 20
+
+
+# Define the size of the test set
+test_size = 0.5
+
+# Calculate the number of examples in the test set
+test_size = int(len(Y) * test_size)
+
+# Split the dataset into a training set and a testing set
+X_train = np.r_[X1[:-test_size//2], X2[:-test_size//2]]
+X_test = np.r_[X1[-test_size//2:], X2[-test_size//2:]]
+Y_train = np.r_[Y1[:-test_size//2], Y2[:-test_size//2]]
+Y_test = np.r_[Y1[-test_size//2:], Y2[-test_size//2:]]
+
 
 # Step 2: Define the SVM class
 class SVM:
@@ -49,10 +71,10 @@ def plot_decision_boundary_testing(svm, X, y):
     def f(x, w, b, c=0):
         return (-w[0] * x - b + c) / w[1]
 
-    plt.scatter(X[:, 0], X[:, 1], c=y)
+    plt.scatter(X[:, 0], X[:, 1], c=y) # color decided by y
     a0 = -4; a1 = f(a0, svm.w, svm.b)
     b0 = 4; b1 = f(b0, svm.w, svm.b)
-    plt.plot([a0,b0], [a1,b1], 'k')
+    plt.plot([a0,b0], [a1,b1], 'k') # decision boundary
 
     a0 = -4; a1 = f(a0, svm.w, svm.b, 1)
     b0 = 4; b1 = f(b0, svm.w, svm.b, 1)
@@ -62,12 +84,13 @@ def plot_decision_boundary_testing(svm, X, y):
     b0 = 4; b1 = f(b0, svm.w, svm.b, -1)
     plt.plot([a0,b0], [a1,b1], 'k--')
 
+    # a0 and a1 define the start point of the line. a0 is the x-coordinate and a1 is the y-coordinate
+    # b0 and b1 define the end point of the line. b0 is the x-coordinate and b1 is the y-coordinate.
+
     plt.show()
 
 # Testing the model 
 
-X_test = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
-Y_test = [-1] * 20 + [1] * 20
 
 # Predict the labels of the test set
 Y_pred = svm.predict(X_test)
